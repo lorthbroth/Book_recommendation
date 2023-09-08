689451337426cb9e2e3680b291fcc853ddaffd1f
Dominic Kgoete <49305704+lorthbroth@users.noreply.github.com>
2023-09-08 15:16:36 +0200

    Delete portal-backend directory (#30)
    refactor

--- portal-backend/src/main/java/com/bmw/model/User.java
+++
@@ -1,9 +0,0 @@
-package com.bmw.model;
-
-import io.quarkus.mongodb.panache.PanacheMongoEntity;
-import io.quarkus.mongodb.panache.common.MongoEntity;
-
-@MongoEntity(collection = "users")
-public class User extends PanacheMongoEntity {
-    public String username;
-}
