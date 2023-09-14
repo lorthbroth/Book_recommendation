012fa3d21b6ec2ed65e84578485aca05a8a9482f
Dominic Kgoete (ext.) <dominic.kgoete@bmwithub.co.za>
2023-09-14 22:18:41 +0200

    Adding frontend and keycloak configs to the project

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  constructor(private readonly keycloak: KeycloakService) {}
+
+  public hasAdminRole: boolean = false;
+
+  ngOnInit(): void {
+      this.hasAdminRole = this.keycloak.getUserRoles().includes('admin');
+  }
+
+  public async logout() {
+    this.keycloak.logout();
+  }
+
+}
